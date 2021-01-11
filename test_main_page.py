80dc73124024ac2ad3703ebec1b35a4185502c29
EgorChueshkov <egor.chueshkov@yandex.com>
2021-01-11 23:18:31 +0300

    realization transition between URL

--- test_main_page.py
+++ test_main_page.py
@@ -2,17 +2,13 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-
-
-def test_guest_is_on_the_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_form()
-    page.should_be_register_form()
-    page.should_be_login_url()
+    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
