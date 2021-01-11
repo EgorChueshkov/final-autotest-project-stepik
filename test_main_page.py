05b4f54c29819846183bddc3d1b4567ad1d45eb2
EgorChueshkov <egor.chueshkov@yandex.com>
2021-01-11 22:59:20 +0300

    add test for LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,18 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+
+
+def test_guest_is_on_the_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_form()
+    page.should_be_register_form()
+    page.should_be_login_url()
